64d8b6d9deb016fed0204140b4e8f5dedc144ff8
Mikko Drocan <mikko.drocan@iki.fi>
2018-07-19 15:28:12 +0300

    added check for shellcheck

--- syntaxchecker.sh
+++ syntaxchecker.sh
@@ -2,10 +2,15 @@
 
 scriptname=$0
 
+shellcheck_exist () {
+    type shellcheck >/dev/null 2>&1 || { echo >&2 "You need to install shellcheck to use this script."; exit 1; }
+}
+
 display_usage () {
 	echo "Usage: $scriptname"
 	echo "Analyzes all files with .sh filetype in the executed directory with shellcheck."
 }
 
 scripts="$(find . -name "*.sh")"
+shellcheck_exist
 shellcheck $scripts
