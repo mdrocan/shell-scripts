c5b55c04cc01fcb032923a36c01e6838ba2856ee
Mikko Drocan <mikko.drocan@iki.fi>
2018-07-25 19:38:37 +0300

    small changes to the functionality

--- osx_wifi_strength.sh
+++ osx_wifi_strength.sh
@@ -3,30 +3,45 @@
 scriptname=$0
 
 display_usage () {
-	echo "Usage: $scriptname [-amount of rounds]"
-	echo "-amount of rounds : Script executed so many times."
+	echo "Usage: $scriptname [::digit::]"
+	echo "digit: Script executed this many times."
 }
 
 counter=0
 
-functionality () {
+test () {
+	date
 	/System/Library/PrivateFrameworks/Apple*.framework/Versions/Current/Resources/airport -I \
 	| awk 'BEGIN{FS="\n"} /channel|CtlRSSI|SSID:/ {print}' | sed -e "s/ //g" \
 	| sed -e 's/agrCtlRSSI/Wifi dBm/' | sed -e '/BSSID/d'
 	sleep 1
 }
 
+if [ $# -gt 1 ]; then
+	echo "Incorrect amount of arguments. Currently accepted arguments listed below."
+	display_usage
+	exit 1
+fi
+
 if [ $# -eq 0 ]; then
 	while true; do 
-	printf "=====""\\n"
-	functionality
-	done
-	exit 0
-else
-	while [ $counter -lt "$1" ]; do
 		printf "=====""\\n"
-		functionality
-		counter=$((counter + 1))
+		test
 	done
 	exit 0
+fi
+
+if [ $# -eq 1 ]; then
+	if [[ "$1" =~ ^[0-9]+$ ]]; then
+		while [ $counter -lt "$1" ]; do
+			printf "=====""\\n"
+			test
+			counter=$((counter + 1))
+		done
+		exit 0
+	else
+		echo "Incorrect input."
+		display_usage
+		exit 1
+	fi
 fi
