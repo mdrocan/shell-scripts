b54f18b68cf63f42da152a319b0f6b4314031244
Mikko Drocan <mikko.drocan@iki.fi>
2018-09-18 11:08:42 +0300

    added check number of images

--- calculate_containers.sh
+++ calculate_containers.sh
@@ -17,6 +17,10 @@ if [ $# -ne 0 ]; then
 	exit 1
 fi
 
+if [[ "$amount_of_images" -eq 0 ]]; then
+	exit 1
+fi
+
 GB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/GB/ {size += $1} END {print size}')"
 MB2GB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/MB/ {size += $1} END {print size/1024}')"
 
