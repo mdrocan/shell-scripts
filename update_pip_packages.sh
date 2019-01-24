91211575a95932e424212eb1cdc48b0515f48e95
Mikko Drocan <mikko.drocan@iki.fi>
2019-01-24 09:52:58 +0200

    small fixes

--- update_pip_packages.sh
+++ update_pip_packages.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 pip2=$(find "/usr/local/Cellar//python@2" -type f -iname pip2)
-pip3=$(find "/usr/local/Cellar/" -type f -iname pip3)
+pip3=$(find "/usr/local/Cellar//python" -type f -iname pip3)
 
 pip2_func() {
+	$pip2 install --upgrade pip
 	echo "Python2 - package check"
 	$pip2 list --outdated --format=freeze | cut -d = -f 1 | xargs -n1 "$pip2" install -U
-	echo "Python2 - packages checked/updated"
 }
 
 pip3_func() {
+	$pip3 install --upgrade pip
 	echo "Python3 - package check"
-	pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 "pip3" install -U
-	echo "Python3 - packages checked/updated"
+	$pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 "$pip3" install -U
 }
 
 if [ -z "$pip2" ]; then 
