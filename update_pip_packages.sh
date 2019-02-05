79ac67dbd6a66e24e32c0002479fa569395d90e0
Mikko Drocan <mikko.drocan@iki.fi>
2019-02-05 10:09:11 +0200

    removed Python2 support from /usr/local/Cellar

--- update_pip_packages.sh
+++ update_pip_packages.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 
-pip2=$(find "/usr/local/Cellar//python@2" -type f -iname pip2)
 pip3=$(find "/usr/local/Cellar//python" -type f -iname pip3)
-
-pip2_func() {
-	$pip2 install --upgrade pip
-	echo "Python2 - package check"
-	$pip2 list --outdated --format=freeze | cut -d = -f 1 | xargs -n1 "$pip2" install -U
-}
 
 pip3_func() {
 	$pip3 install --upgrade pip
@@ -15,13 +8,10 @@ pip3_func() {
 	$pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 "$pip3" install -U
 }
 
-if [ -z "$pip2" ]; then 
-	echo "pip2 is unset";
-	pip3_func
-	exit 0
-else 
-	echo "Python2 and pip2 found from: '$pip2'";
-	pip2_func
+if [ -z "$pip3" ]; then 
+	echo "pip3 is unset";
+	exit 1
+lse 
 	echo "Python3 and pip3 found from: '$pip3'";
 	pip3_func
 	exit 0
