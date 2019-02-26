273062f4a6f8228973c4c34ddf0e6761cf0ec302
Mikko Drocan <mikko.drocan@iki.fi>
2019-02-26 16:01:01 +0200

    correcting process

--- update_pip_packages.sh
+++ update_pip_packages.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+pip3=$(find "/usr/local/bin/" -type f -iname pip3)
+
 pip3_func() {
-	pip3 install --upgrade pip
+	$pip3 install --upgrade pip
 	echo "Python3 - package check"
-	pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 pip3 install -U
+	$pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 "$pip3" install -U
 }
 
-if [ -z pip3 ]; then 
+if [ -z "$pip3" ]; then 
 	echo "pip3 is unset";
 	exit 1
 else 
