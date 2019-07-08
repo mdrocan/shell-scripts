ddf6b6c054fa0ac335aa7db589502decbee39e9a
Mikko Drocan <mikko.drocan@iki.fi>
2019-07-08 13:21:48 +0300

    removed unnecessary stuff
    	modified:   info_pip_packages.sh
    	modified:   update_pip_packages.sh

--- info_pip_packages.sh
+++ info_pip_packages.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-pip3=$(find "/usr/local/bin/" -type f -iname pip3)
-#pip3=$(find "/usr/local/Cellar/python/" -type f -iname pip3)
+
+pip3=$(find "/usr/local/bin/" -iname pip3)
 
 pip3_func() {
 	$pip3 install --upgrade pip
-	$pip3 list | cut -d " " -f 1 | sed 1,2d | xargs pip show
+	$pip3 list | cut -d " " -f 1 | sed 1,2d | xargs pip3 show
 }
 
 if [ -z "$pip3" ]; then 
