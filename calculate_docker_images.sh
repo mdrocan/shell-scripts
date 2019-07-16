b9237453ce23ca0d35342bdb1968327735e50a98
Mikko Drocan <mikko.drocan@iki.fi>
2019-07-16 16:53:45 +0300

    small enchancement to calculation logic

--- calculate_docker_images.sh
+++ calculate_docker_images.sh
@@ -2,13 +2,15 @@
 
 scriptname=$0
 
+amount_of_images="$(docker images | tail -n +2 | awk 'END{print NR}')"
+GB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/GB/ {size += $1} END {print size}')"
+MB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/MB/ {size += $1} END {print size}')"
+MB2GB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/MB/ {size += $1} END {print size/1024}')"
+
 display_usage () {
-	echo "Script info:"
 	echo "Calculates the space used by containers."
 	echo "Usage: $scriptname"
 	}
-
-amount_of_images="$(docker images | tail -n +2 | awk 'END{print NR}')"
 
 if [ $# -ne 0 ]; then
 	echo ""
@@ -20,15 +22,16 @@ if [ $# -ne 0 ]; then
 fi
 
 if [[ "$amount_of_images" -eq 0 ]]; then
+	echo "Couldn't find any docker images."
 	exit 0
 fi
 
-GB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/GB/ {size += $1} END {print size}')"
-MB2GB="$(docker images | tail -n +2 | awk '{print $7}' | awk '/MB/ {size += $1} END {print size/1024}')"
-
 echo "Container images in total:" "$amount_of_images"
-
 if [ -z "$GB" ]; then
+	if [[ "$MB2GB" = 0.* ]]; then
+	 echo "Used space: ""$MB"" MB"
+	 exit 0
+	fi
 	echo "Used space: ""$MB2GB"" GB"
 	exit 0
 else
