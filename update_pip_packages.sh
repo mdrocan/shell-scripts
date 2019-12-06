a35827712b6785ac0151bfb0620385f5e843d648
Mikko Drocan <mikko.drocan@iki.fi>
2019-12-06 16:29:42 +0200

    cleaner output

--- update_pip_packages.sh
+++ update_pip_packages.sh
@@ -2,15 +2,27 @@
 
 pip3=$(find "/usr/local/bin/" -iname pip3)
 
-pip3_func() {
-	$pip3 install --upgrade pip
+pip_str="Requirement already up-to-date"
+
+pip3_package_update() {
 	$pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 "$pip3" install -U
+}
+
+pip3_update() {
+	if [[ $($pip3 install --upgrade pip) == *"$pip_str"* ]];
+		then
+			pip3_package_update
+			exit 0
+		else
+			pip3_package_update
+			exit 0
+		fi
 }
 
 if [ -z "$pip3" ]; then 
 	echo "pip3 is unset";
 	exit 1
 else 
-	pip3_func
+	pip3_update
 	exit 0
 fi
