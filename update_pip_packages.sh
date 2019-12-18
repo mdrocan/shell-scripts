32f83ff583b81d53d7f5e995421015005de86e6b
Mikko Drocan <mikko.drocan@iki.fi>
2019-12-18 13:25:52 +0200

    small fix for checking

--- update_pip_packages.sh
+++ update_pip_packages.sh
@@ -2,14 +2,14 @@
 
 pip3=$(find "/usr/local/bin/" -iname pip3)
 
-pip_str="Requirement already up-to-date"
+pip_str="Requirement already up-to-date:"
 
 pip3_package_update() {
 	$pip3 list --outdated --format=freeze --no-cache-dir | cut -d = -f 1 | xargs -n1 "$pip3" install -U
 }
 
 pip3_update() {
-	if [[ $($pip3 install --upgrade pip) == *"$pip_str"* ]];
+	if [[ $($pip3 install --upgrade pip) == "$pip_str" ]];
 		then
 			pip3_package_update
 			exit 0
