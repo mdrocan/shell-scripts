1256aed85966b5742a3fb9a2fb050a8d2fc60154
Mikko Drocan <mikko.drocan@iki.fi>
2020-05-01 14:50:57 +0300

    possible to check a directory which is given as an argument

--- syntaxchecker.sh
+++ syntaxchecker.sh
@@ -13,4 +13,33 @@ display_usage () {
 
 shellcheck_exist
 
-find . -iname "*\.sh" -print0 | xargs -0 shellcheck
+if [ $# -eq 0 ]; then
+	find . -name "*\.sh" -print0 | xargs -0 shellcheck
+fi
+
+if [ $# -eq 1 ]; then
+	if [ -e "$1" ]; then
+		find "$1" -type f -name "*\.sh" -print0 2>/dev/null | xargs -0 shellcheck
+	else
+	while :
+    do
+        case $1 in
+		-h)
+            echo ""
+            display_usage
+            exit 0
+            ;;
+        --help)
+            echo ""
+            display_usage
+            exit 0
+            ;;
+        *)
+            echo ""
+            echo "Incorrect parameter in use. Correct parameters given in the example below."
+            display_usage
+            exit 1
+        esac
+    done
+	fi
+fi
