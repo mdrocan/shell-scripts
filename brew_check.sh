f5b05948e3b758b03f26346699b19778f248de58
Mikko Drocan <mikko.drocan@iki.fi>
2020-11-26 14:10:04 +0200

    improved readability

--- brew_check.sh
+++ brew_check.sh
@@ -6,22 +6,20 @@ brew_exist () {
     type brew >/dev/null 2>&1 || { echo >&2 "Homebrew needed to utilize this program. Installation instruction: https://brew.sh/ "; exit 1; }
 }
 
+# preparation part
 brew_exist
-brew doctor
 brew update
+brew_outdated=$(brew outdated | wc -l)
+brew_cask_outdated=$(brew outdated --cask --greedy | wc -l)
 
 display_usage () {
     echo "Usage: $scriptname []"
-    echo "[empty]: Check available formulae and application updates."
-    echo "-i : Install upgradable formulae and applications."
+    echo "[empty]: Check available updates (formulae and application)."
+    echo "-i : Install updates (formulae and applications)."
     echo "-clean : Cleanup cache."
-    echo "-h / --help : Help documentation."
+    echo "-h / --help : Help documentation (this doc)."
     echo ""
 }
-
-brew_outdated=$(brew outdated | wc -l)
-
-brew_cask_outdated=$(brew outdated --cask --greedy | wc -l)
 
 list_updates () {
     if [ "$brew_outdated" -eq 0 ] && [ "$brew_cask_outdated" -eq 0 ]
@@ -49,6 +47,7 @@ list_updates () {
         fi
 }
 
+#logical part
 if [ $# -eq 0 ]; then
   list_updates
   exit 0
@@ -64,9 +63,8 @@ else
             -i)
                 brew update --greedy
                 brew upgrade
-                brew outdated --cask --greedy | cut -d = -f 1 | xargs -n1 brew cask reinstall
+                brew outdated --cask --greedy | cut -d = -f 1 | xargs -n1 brew upgrade --cask
                 brew cleanup -s && rm -rf $(brew --cache)
-                brew doctor
                 exit 0
                 ;;
             -h)
