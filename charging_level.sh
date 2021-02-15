98bb9f945779b56bcfd868cb0bd155b3b315abfc
Mikko Drocan <mikko.drocan@iki.fi>
2021-02-15 13:32:18 +0200

    improved check for discharging state

--- charging_level.sh
+++ charging_level.sh
@@ -4,6 +4,8 @@ batterylevel=$(pmset -g batt | sed 1d | awk '{print$3}' | sed 's/..$//')
 
 charge_state=$(pmset -g batt | sed 1d | awk '{print$4}' | sed 's/.$//')
 
+charge_state2=$(pmset -g batt | sed 1d | awk '{for (i=4; i<NF; i++) printf $i " "; print $NF}')
+
 notification30 () {
 osascript -e 'display notification "Battery level below 30%." with title "Check charging!"'
 }
@@ -18,7 +20,7 @@ else
   exit 0
 fi
 
-if [ "$charge_state" = "discharging" ]; then
+if [ "$charge_state" = "discharging" ] || [ "$charge_state2" = "AC attached; not charging present: true" ]; then
   charge_state_inactive
 else
   exit 0
