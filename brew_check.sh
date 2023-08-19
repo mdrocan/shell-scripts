c99e594efd2fc84a1d6e6d0d7c9cf7abb8edb7b4
Mikko Drocan <mikko.drocan@iki.fi>
2023-08-19 08:58:03 +0300

    fixes based on shfmt

--- brew_check.sh
+++ brew_check.sh
@@ -5,7 +5,7 @@ scriptname=$0
 brew_exist() {
     type brew >/dev/null 2>&1 || {
         echo >&2 "Homebrew needed to utilize this program. Installation instruction: https://brew.sh/ "
-        exit 1;
+        exit 1
     }
 }
 
@@ -23,7 +23,7 @@ brew_exist
 
 #faster help section
 while [ $# -eq 1 ]; do
-  case "$1" in
+    case "$1" in
     -h | --help)
         display_usage
         exit 0
@@ -39,7 +39,7 @@ while [ $# -eq 1 ]; do
         display_usage
         exit 1
         ;;
-  esac
+    esac
 done
 
 while [ $# -gt 1 ]; do
@@ -53,26 +53,26 @@ brew update
 brew_outdated_amount=$(brew outdated | wc -l)
 brew_cask_outdated_amount=$(brew outdated --cask --greedy | wc -l)
 
-list_updates () {
+list_updates() {
     if [ "$brew_outdated_amount" -eq 0 ] && [ "$brew_cask_outdated_amount" -eq 0 ]; then
+        exit 0
+    else
+        if [ "$brew_outdated_amount" -eq 0 ]; then
+            printf '\nAvailable applications:\n'
+            brew outdated --cask --greedy
             exit 0
         else
-            if [ "$brew_outdated_amount" -eq 0 ]; then
-                    printf '\nAvailable applications:\n'
-                    brew outdated --cask --greedy
-                    exit 0
-                else
-                    printf '\nAvailable formulae:\n'
-                    brew outdated
-                    if [ "$brew_cask_outdated_amount" -ne 0 ]; then
-                            printf '\nAvailable applications:\n'
-                            brew outdated --cask --greedy
-                            exit 0
-                        else
-                            exit 0
-                    fi
-#                exit 0
+            printf '\nAvailable formulae:\n'
+            brew outdated
+            if [ "$brew_cask_outdated_amount" -ne 0 ]; then
+                printf '\nAvailable applications:\n'
+                brew outdated --cask --greedy
+                exit 0
+            else
+                exit 0
             fi
+            #                exit 0
+        fi
     fi
 }
 
@@ -81,19 +81,19 @@ if [ $# -eq 0 ]; then
     list_updates
 #  exit 0
 else
-  if [ $# -eq 1 ]; then
-    while :; do
-        case $1 in
+    if [ $# -eq 1 ]; then
+        while :; do
+            case $1 in
             -clean)
                 brew cleanup -s && rm -rf "$(brew --cache)"
                 exit 0
                 ;;
             -i | --install)
                 if [ "$brew_outdated_amount" -eq 0 ] && [ "$brew_cask_outdated_amount" -eq 0 ]; then
-                        exit 0
-                    else
-                        brew upgrade
-                        brew outdated --cask --greedy | cut -d = -f 1 | xargs -n1 brew upgrade --cask
+                    exit 0
+                else
+                    brew upgrade
+                    brew outdated --cask --greedy | cut -d = -f 1 | xargs -n1 brew upgrade --cask
                 fi
                 brew cleanup -s && rm -rf "$(brew --cache)"
                 exit 0
